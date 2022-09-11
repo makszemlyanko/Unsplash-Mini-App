280b4f9c2955fac875135f417a78c2dd6d4d1461
kokosDev <maks.kokos.dev@gmail.com>
2022-09-11 18:15:33 +0300

    initial commit

---
+++ UnsplashMiniApp/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  UnsplashMiniApp
+//
+//  Created by Maks Kokos on 11.09.2022.
+//
+
+import UIKit
+
+class MainTabBarController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink 
+    }
+}
